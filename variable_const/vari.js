0143122a05c216a9a25e659e45a1cb86445b07d5
SIDDHI TRIPATHI <eshutripathi1611@gmail.com>
2026-07-12 10:54:30 +0000

    variables and constants

---
+++ variable_const/vari.js
@@ -0,0 +1,11 @@
+const accid = 1442
+let accemail = "sid@gmail.com"
+var accpassword = "12345" // prefred not to use because of issue in block scope and functional scope
+
+acccity = "Hyderabad"
+
+let accstatus;
+
+console.log(accid,accemail);
+
+console.table([accemail,accid,accpassword,acccity,accstatus])
