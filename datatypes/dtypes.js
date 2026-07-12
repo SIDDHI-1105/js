54a636767cc203b77ff78835caff3336d96c16b8
SIDDHI TRIPATHI <eshutripathi1611@gmail.com>
2026-07-12 12:33:07 +0000

    datatypes

---
+++ datatypes/dtypes.js
@@ -0,0 +1,20 @@
+ "use strict" // treats all js code as newer version
+
+ // alert(2+3) we are using node.js not browser
+
+ console.log(3+3)
+ console.log("sid")
+
+ let name = " siddhi"
+ let age = 20
+ let isloggedin = true
+let state = null
+
+//symbol => for uniqieness 
+
+console.log(typeof age)
+
+console.log(typeof undefined)
+console.log(typeof null)
+
+
