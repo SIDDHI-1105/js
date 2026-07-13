91fce7acc22163fb627fd963a3e95468fedb76ef
SIDDHI TRIPATHI <eshutripathi1611@gmail.com>
2026-07-13 17:03:00 +0000

    string

---
+++ string/str.js
@@ -0,0 +1,16 @@
+const name = " sid"
+const name2 = " esha"
+
+console.log(`my name is ${name} and my nik name is ${name2}`)
+
+const gamename = new String("sidhu")
+
+console.log(gamename[0])
+console.log(gamename.__proto__)
+
+console.log(gamename.length)
+
+console.log(gamename.toUpperCase())
+
+console.log(gamename.charAt(2))
+
