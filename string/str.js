970db00c017681b1e2fccc857649d46adf1dbcb3
SIDDHI TRIPATHI <eshutripathi1611@gmail.com>
2026-07-15 03:57:44 +0000

    string

--- string/str.js
+++ string/str.js
@@ -14,3 +14,31 @@ console.log(gamename.toUpperCase())
 
 console.log(gamename.charAt(2))
 
+console.log(gamename.indexOf('d'))
+
+console.log(gamename.substring(0,3))
+
+const anotherstring = gamename.slice(0,3)
+const naam = "  sid  "
+console.log(naam.trim())
+
+const url = "http://sidz.com/sid%20tripathi"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('sidd'))
+
+console.log(url.split('-'))
+
+
+
+
+
+
+
+
+
+
+
+
+
